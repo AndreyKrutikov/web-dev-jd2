c6996538158790c53a87529c791d7b63c3bf6160
Andrey Krutikov <andrusha.krutikov@gmail.com>
2022-07-20 14:16:44 +0400

    Changes in Faculty enum constructor

--- src/main/java/by/krutikov/Faculty.java
+++ src/main/java/by/krutikov/Faculty.java
@@ -1,14 +1,20 @@
 package by.krutikov;
 
 public enum Faculty {
-    ABC("abc"), DEF("def");
+    ABC("abc", 1), DEF("def", 2);
     private String name;
+    private int code;
 
-    Faculty(String name) {
+    Faculty(String name, int code) {
         this.name = name;
+        this.code = code;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getCode() {
+        return code;
     }
 }
