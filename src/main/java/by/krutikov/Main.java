687f148a3cc0fca89520a333eeb4ce385fa81c9a
Andrey Krutikov <andrusha.krutikov@gmail.com>
2022-07-27 17:49:42 +0400

    Webapp structure resolved.

--- src/main/java/by/krutikov/Main.java
+++ src/main/java/by/krutikov/Main.java
@@ -3,7 +3,7 @@ package by.krutikov;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main implements Runnable{
+public class Main implements Runnable {
     static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
@@ -11,10 +11,18 @@ public class Main implements Runnable{
         logger.info("Main1 thread started");
         new Thread(new Main2()).start();
         logger.info("Main2 thread started");
+
+        int[] num = {1, 2, 3, 4, 5, 6};
+        for (int i : num) {
+            System.out.println(i);
+        }
+
+
     }
+
     @Override
     public void run() {
-        for(int i =0; i<8 ; i++){
+        for (int i = 0; i < 8; i++) {
             logger.info("running 1");
             try {
                 Thread.sleep(1000);
@@ -24,11 +32,13 @@ public class Main implements Runnable{
         }
     }
 }
-class Main2 implements Runnable{
+
+class Main2 implements Runnable {
     static final Logger logger = LogManager.getLogger(Main2.class);
+
     @Override
     public void run() {
-        for(int i =0; i<8 ; i++){
+        for (int i = 0; i < 8; i++) {
             logger.info("running 2");
             try {
                 Thread.sleep(1000);
